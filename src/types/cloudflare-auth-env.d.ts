895ea7062e09ea208e16c1cbfd368d6fad635752
metaloozee <ayanparkar123@gmail.com>
2026-03-29 20:08:42 +0200

    chore: types

---
+++ src/types/cloudflare-auth-env.d.ts
@@ -0,0 +1,10 @@
+
+
+declare global {
+  interface CloudflareEnv {
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    NEXT_PUBLIC_BETTER_AUTH_URL: string;
+    BETTER_AUTH_SECRET: string;
+  }
+}
