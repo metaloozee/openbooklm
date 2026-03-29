8eefc77d002f2be562b69aa30a10a672e94e1e5b
metaloozee <ayanparkar123@gmail.com>
2026-03-29 16:37:47 +0200

    fix: build err

--- src/lib/auth/auth-client.ts
+++ src/lib/auth/auth-client.ts
@@ -1,7 +1,5 @@
 import { createAuthClient } from "better-auth/react";
-import { env } from "cloudflare:workers";
 
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: env.BETTER_AUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
 });
