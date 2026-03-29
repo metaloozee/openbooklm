6bd05f3b1189564274c00dad924f074dbb68f7b1
metaloozee <ayanparkar123@gmail.com>
2026-03-29 14:53:18 +0200

    chore: google auth

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -1,5 +1,6 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { env } from "cloudflare:workers";
 
 import { db } from "@/lib/db";
 
@@ -7,4 +8,10 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
+  socialProviders: {
+    google: {
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    },
+  },
 });
