81897b2599e1653ff758447272fe02f2484dfa23
metaloozee <ayanparkar123@gmail.com>
2026-04-06 20:49:20 +0200

    chore: base url in env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,17 +3,15 @@ import * as z from "zod";
 
 export const env = createEnv({
   client: {
-    NEXT_PUBLIC_BETTER_AUTH_URL: z.preprocess(
+    //
+  },
+  experimental__runtimeEnv: process.env,
+  server: {
+    BETTER_AUTH_SECRET: z.string().min(1),
+    BETTER_AUTH_URL: z.preprocess(
       (str) => process.env.VERCEL_URL ?? str,
       process.env.VERCEL_URL ? z.string().min(1) : z.string().url()
     ),
-  },
-  experimental__runtimeEnv: {
-    ...process.env,
-    NEXT_PUBLIC_BETTER_AUTH_URL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
-  },
-  server: {
-    BETTER_AUTH_SECRET: z.string().min(1),
     BLOB_READ_WRITE_TOKEN: z.string().min(1),
     DATABASE_URL: z.string().url(),
     GOOGLE_CLIENT_ID: z.string().min(1),
